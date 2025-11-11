264063d5a564cbdbc37555ed94780e9a64aae72a
Pranav Joshi <pranavjoshi2893@gmail.com>
2025-11-11 16:20:34 +0530

    timing problem resolved

--- problem10/main.go
+++ problem10/main.go
@@ -39,46 +39,92 @@
 // 	stop()
 // }
 
+// package main
+
+// import (
+// 	"fmt"
+// 	"runtime"
+// 	"time"
+// )
+
+// func StartWorker() (stopChan, doneChan chan struct{}) {
+// 	stop := make(chan struct{})
+// 	done := make(chan struct{})
+
+// 	go func() {
+// 		defer close(done)
+// 		for {
+// 			select {
+// 			case <-stop:
+// 				return
+// 			default:
+// 				fmt.Println("woof...")
+// 				time.Sleep(time.Millisecond * 500)
+// 			}
+// 		}
+// 	}()
+
+// 	return stop, done
+// }
+
+// func main() {
+// 	stop1, done1 := StartWorker()
+// 	stop2, done2 := StartWorker()
+
+// 	fmt.Println(runtime.NumGoroutine())
+
+// 	time.Sleep(time.Second * 5)
+// 	close(stop1)
+// 	close(stop2)
+// 	<-done1
+// 	<-done2
+
+// 	fmt.Println(runtime.NumGoroutine())
+// 	fmt.Println("let me SLEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEp... It's 4 a.m")
+// }
+
 package main
 
 import (
 	"fmt"
-	"time"
 	"runtime"
+	"sync"
+	"time"
 )
 
-func StartWorker() (stopChan, doneChan chan struct{}) {
+func startWorker() (func(), <-chan struct{}) {
+	var wg sync.WaitGroup
+
 	stop := make(chan struct{})
 	done := make(chan struct{})
 
+	wg.Add(1)
+
 	go func() {
+		defer wg.Done()
 		defer close(done)
 		for {
 			select {
 			case <-stop:
 				return
-			default:
-				fmt.Println("woof...")
-				time.Sleep(time.Millisecond * 500)
+			case <-time.After(500 * time.Millisecond):
+				fmt.Println("running")
 			}
 		}
 	}()
 
-	return stop, done
+	return func() {
+		close(stop)
+		wg.Wait()
+	}, done
 }
 
 func main() {
-	stop1, done1 := StartWorker()
-	stop2, done2 := StartWorker()
 
+	stop, done := startWorker()
 	fmt.Println(runtime.NumGoroutine())
-
-	time.Sleep(time.Second * 5)
-	close(stop1)
-	close(stop2)
-	<-done1
-	<-done2
-
+	time.Sleep(5 * time.Second)
+	stop()
+	<-done
 	fmt.Println(runtime.NumGoroutine())
-	fmt.Println("let me SLEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEp... It's 4 a.m")
 }
